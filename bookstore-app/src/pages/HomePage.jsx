0abdd69fe77b8924d8c744489e0a0e18b764aff1
porawat7 <chawrainak_p@silpakorn.edu>
2025-09-18 05:26:17 +0000

    bookstore101

---
+++ bookstore-app/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import{Link} from 'react-router-dom';
+import './style/HomePage.css';
+
+const HomePage = () => {
+        return(
+            <div>
+                 <h1> Welcome to the Bookstore</h1>
+                 <p>This is the home page of the bookstore application</p>   
+            </div>
+
+);
+
+}
+export default HomePage;
